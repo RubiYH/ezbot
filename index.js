4ef43f8c8d6325d84b050c6375845d7c5770fe04
RubiYH <89332373+RubiYH@users.noreply.github.com>
2021-08-22 15:07:47 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const ez = require("./ez.json");
+
+client.on("message", async (message) => {
+  if (message.content.toLowerCase().match(/\b(ez)\b/g)) {
+    try {
+      const webhooks = await message.channel.fetchWebhooks();
+      const webhook = webhooks.find((w) => w.name === "ezBot");
+
+      let txt = ez[Math.floor(Math.random() * ez.length)];
+
+      //create webhook if not exists
+      if (!webhook) {
+        await message.channel
+          .createWebhook("ezBot", {
+            avatar: client.user.displayAvatarURL(),
+          })
+          .then(async () => {
+            console.log(`Created a webhook in; ${message.channel.name}`);
+            message.channel.send(
+              "*It seems that the webhook is missing on this channel, so I've added one!*"
+            );
+
+            message
+              .delete()
+              .catch((err) => console.log(`Error on deletion: ${err}`));
+          });
+      } else {
+        //send ez message with fake user
+
+        webhook.send(txt, {
+          username: message.member.displayName,
+          avatarURL: message.author.displayAvatarURL(),
+        });
+
+        message
+          .delete()
+          .catch((err) => console.log(`Error on deletion: ${err}`));
+      }
+    } catch (err) {
+      console.log(`Webhook error: ${err}`);
+    }
+  }
+});
+
+//when bot joins
+client.on("guildCreate", (guild) => {
+  console.log(`Joined server: ${guild.name}`);
+  //send welcome message to a random channel
+
+  var hello_embed = new Discord.MessageEmbed()
+    .setTitle("\\❤️ Thanks for inviting me!")
+    .addField(
+      "This bot will replace any messages that include `ez` with random phrases from Hypixel.\nEnjoy!",
+      "\u200b"
+    )
+    .setFooter("By Rubi#3719")
+    .setColor("GREEN");
+
+  const first_channel = guild.channels.cache
+    .filter((c) => {
+      return c.manageable && c.type === "text";
+    })
+    .first();
+
+  if (first_channel) first_channel.send(hello_embed);
+
+  //add webhooks in all channels
+  var i = 0;
+  guild.channels.cache.forEach((channel) => {
+    if (channel.type !== "text") return;
+
+    channel.createWebhook("ezBot", {
+      avatar: client.user.displayAvatarURL(),
+    });
+    i++;
+    if (i == guild.channels.cache.filter((c) => c.type == "text").size)
+      return console.log("Successfully added webhooks to all text channels.");
+  });
+});
+
+//when channel created
+client.on("channelCreate", (channel) => {
+  channel.createWebhook("ezBot", {
+    avatar: client.user.displayAvatarURL(),
+  });
+
+  console.log(`Added webhook to the channel; ${channel.name}`);
+});
+
+//login
+client.login(config.token).then(() => console.log("Logged in."));
